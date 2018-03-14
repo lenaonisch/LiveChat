39c658ffc505d62f75d4e6e95d50be3a9a3dc468
lenaonisch <elenaonishhenk0@gmail.com>
2018-03-15 00:41:09 +0200

    recreate initial migrations after successful connection to database + split IdentityModels.cs into ApplicationDbContext.cs and ApplicationUser.cs

--- LiveChat/Migrations/201803102027538_Initial.Designer.cs
+++ LiveChat/Migrations/201803102027538_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace LiveChat.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class InitialModel : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialModel));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201803072120331_InitialModel"; }
+            get { return "201803102027538_Initial"; }
         }
         
         string IMigrationMetadata.Source
--- LiveChat/Migrations/201803102027538_Initial.cs
+++ LiveChat/Migrations/201803102027538_Initial.cs
@@ -3,7 +3,7 @@ namespace LiveChat.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialModel : DbMigration
+    public partial class Initial : DbMigration
     {
         public override void Up()
         {
