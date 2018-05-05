64021a257ef98ed3a1de50179b514a7e434c0ed6
Andrey123456789 <dnepr_andrey@yahoo.com>
2018-05-05 23:13:31 +0300

    Remote Chat - without Razor html and javascript only
    Chat now can work on remote domain

---
+++ CustomerChat/Scripts/LiveChat.js
@@ -0,0 +1,39 @@
+﻿function EmbedChat(CentralChatHub) {
+    $("[data-toggle=popover]").popover({ html: true, container: "#LiveChatWrapper" });
+    $("#StartChat").click(function () {
+		$.support.cors = true;
+        // Reference the auto-generated proxy for the hub.
+        $.connection.hub.url = CentralChatHub + '/signalr';
+        var chat = $.connection.chatHub;
+        // Create a function that the hub can call back to display messages.
+        //RECEIVE
+        chat.client.addNewMessageToPage = function (name, group, message) {
+            // Add the message to the page.
+            $('#discussion').append('<dt><strong>' + htmlEncode(name)
+                + '</strong>: ' + htmlEncode(message) + '</dt>');
+        };
+
+        // Set initial focus to message input box.
+        $('#message').focus();
+        // Start the connection.
+        $.connection.hub.start().done(function () {
+            $('#sendmessage').click(function () {
+                // Call the Send method on the hub.
+                //SEND
+                chat.server.send($('#message').val());
+                // Clear text box and reset focus for next comment.
+                $('#message').val('').focus();
+            });
+
+            $("#StartChat").click(function () {
+                $.connection.hub.stop();
+            });
+        });
+    });
+}
+
+// This optional function html-encodes messages for display in the page.
+function htmlEncode(value) {
+    var encodedValue = $('<div />').text(value).html();
+    return encodedValue;
+}
